176090b2973d2d2af9820ca18b2d1722bb950ea5
Paolo Moroni <paolo@codedby.pm>
2019-12-27 02:27:49 +0100

    Update project ID

--- mongo/create.go
+++ mongo/create.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"fmt"
 	"time"
-	
+
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -17,7 +17,11 @@ func Create(dbName string, collectionName string, document interface{}) (interfa
 	mongoContext, mongoCancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer mongoCancel()
 
-	mongoURI, mongoURIError := generateURI()
+	const keyRingName = "agora-key-ring"
+	const keyName = "agora-crypto-key"
+	const projectID = "agora-polis"
+
+	mongoURI, mongoURIError := generateURI(projectID, keyRingName, keyName)
 	if mongoURIError != nil {
 		return nil, fmt.Errorf("Error: could not generate Mongo URI (%s)", mongoURIError)
 	}
--- mongo/generateURI.go
+++ mongo/generateURI.go
@@ -5,17 +5,17 @@ import (
 	secretmanager "github.com/codedbypm/gcloud-secret-manager/secretmanager"
 )
 
-func generateURI() (string, error) {
+func generateURI(projectID string, keyRingName string, keyName string) (string, error) {
 	const mongoUserSecretName = "agora-secret-mongo-user"
 	const mongoPassSecretName = "agora-secret-mongo-pass"
-	const keyID = "projects/agora-262523/locations/europe-west1/keyRings/agora-key-ring/cryptoKeys/agora-key/cryptoKeyVersions/latest"
+	const keyID = fmt.Sprintf("projects/%s/locations/europe-west1/keyRings/%s/cryptoKeys/%s/cryptoKeyVersions/latest", projectID, keyRingName, keyName)
 
-	mongoUser, err := secretmanager.DecryptSecretSymmetric(mongoUserSecretName, "agora-262523", keyID)
+	mongoUser, err := secretmanager.DecryptSecretSymmetric(mongoUserSecretName, projectID, keyID)
 	if err != nil {
 		return "", fmt.Errorf("Error: could not retrieve secret %s (%s)", mongoUserSecretName, err)
 	}
 
-	mongoPass, err := secretmanager.DecryptSecretSymmetric(mongoPassSecretName, "agora-262523", keyID)
+	mongoPass, err := secretmanager.DecryptSecretSymmetric(mongoPassSecretName, projectID, keyID)
 	if err != nil {
 		return "", fmt.Errorf("Error: could not retrieve secret %s (%s)", mongoPassSecretName, err)
 	}
